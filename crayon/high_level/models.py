d28d6c2ae04202c198a0c1233b7ff70427f8146c
Gaetan Chapput <chapputgaetan@gmail.com>
2024-09-26 15:02:32 +0200

    modif class Stock

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -13,7 +13,7 @@ class Ville(models.Model):
 
 class Local(models.Model):
     nom = models.CharField(max_length=100)
-    ville = models.ForeignKey(Ville, on_delete=models.PROTECT)
+    ville = models.ForeignKey(Ville, blank=True, null=True, on_delete=models.PROTECT)
     surface = models.IntegerField()
 
     class Meta:
@@ -57,14 +57,23 @@ class Ressource(Objet):
 
 
 class QuantiteRessource(models.Model):
-    ressource = models.ForeignKey(Ressource, on_delete=models.CASCADE)
+    ressource = models.ForeignKey(
+        Ressource, blank=True, null=True, on_delete=models.CASCADE
+    )
     quantite = models.IntegerField()
+
+    def __str__(self):
+        return self.nom
 
 
 class Etape(models.Model):
     nom = models.CharField(max_length=100)
-    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
-    quantite_ressource = models.ForeignKey(QuantiteRessource, on_delete=models.PROTECT)
+    machine = models.ForeignKey(
+        Machine, blank=True, null=True, on_delete=models.CASCADE
+    )
+    quantite_ressource = models.ForeignKey(
+        QuantiteRessource, blank=True, null=True, on_delete=models.PROTECT
+    )
     duree = models.IntegerField()
     etape_suivante = models.ForeignKey(
         "self", null=True, on_delete=models.CASCADE, blank=True
@@ -75,9 +84,16 @@ class Etape(models.Model):
 
 
 class Stock(models.Model):
-    ressource = models.ForeignKey(Ressource, on_delete=models.CASCADE)
+    ressource = models.ForeignKey(
+        Ressource, blank=True, null=True, on_delete=models.CASCADE
+    )
     nombre = models.IntegerField()
+
+    def __str__(self):
+        return self.ressource.nom + " : {}".format(self.nombre)
 
 
 class Produit(Objet):
-    premiere_etape = models.ForeignKey(Etape, on_delete=models.CASCADE)
+    premiere_etape = models.ForeignKey(
+        Etape, blank=True, null=True, on_delete=models.CASCADE
+    )
