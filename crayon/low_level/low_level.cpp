c9831cc2a6c85882325e19ca13078fb0c797ddcd
Gaetan Chapput <chapputgaetan@gmail.com>
2024-10-02 11:57:03 +0200

    ajout autres classes low_level.cpp

--- crayon/low_level/low_level.cpp
+++ crayon/low_level/low_level.cpp
@@ -14,18 +14,72 @@ class Ville {
       : _nom{nom}, _code_postal{code_postal}, _prix_m_2{prix_m_2} {}
   friend std::ostream& operator<<(std::ostream& out, const Ville& ville) {
     return out << ville._nom << " : " << ville._code_postal << ", "
-               << ville._prix_m_2 << " m^2";
+               << ville._prix_m_2 << " euros/m^2";
   }
   Ville(int id) {
     std::string url = "http://localhost:8000/ville/" + std::to_string(id);
     cpr::Response r = cpr::Get(cpr::Url{url});
     // r.status_code;
-    // r.header["content-type"];
-    // r.text;
     nlohmann::json j = nlohmann::json::parse(r.text);
     _nom = j["nom"];
     _code_postal = j["code_postal"];
     _prix_m_2 = j["prix_m_2"];
+  }
+
+  auto get_nom() const -> std::string { return _nom; }
+};
+
+class Machine {
+  std::string _nom;
+  int _prix;
+  int _n_serie;
+
+ public:
+  Machine(std::string nom, int prix, int n_serie)
+      : _nom{nom}, _prix{prix}, _n_serie{n_serie} {}
+  friend std::ostream& operator<<(std::ostream& out, const Machine& machine) {
+    return out << machine._nom << " | numero : " << machine._n_serie
+               << " | prix : " << machine._prix << " euros";
+  }
+  Machine(int id) {
+    std::string url = "http://localhost:8000/machine/" + std::to_string(id);
+    cpr::Response r = cpr::Get(cpr::Url{url});
+    // r.status_code;
+    nlohmann::json j = nlohmann::json::parse(r.text);
+    _nom = j["nom"];
+    _prix = j["prix"];
+    _n_serie = j["n_serie"];
+  }
+
+  auto get_nom() const -> std::string { return _nom; }
+};
+
+class Usine {
+  std::string _nom;
+  std::unique_ptr<Ville> _ville;
+  int _surface;
+  std::unique_ptr<Machine> _machines;
+
+ public:
+  Usine(std::string nom, std::unique_ptr<Ville> ville, int surface,
+        std::unique_ptr<Machine> machine)
+      : _nom{nom},
+        _ville{ville->get_nom()},
+        _surface{surface},
+        _machines{machine->get_nom()} {}
+  friend std::ostream& operator<<(std::ostream& out, const Usine& usine) {
+    return out << usine._nom << " : " << usine._ville._nom << ", "
+               << usine._surface << " m^2" << usine._machines.nom;
+  }
+  Usine(int id) {
+    std::string url = "http://localhost:8000/usine/" + std::to_string(id);
+    cpr::Response r = cpr::Get(cpr::Url{url});
+    // r.status_code;
+    nlohmann::json j = nlohmann::json::parse(r.text);
+    _nom = j["nom"];
+    _ville = j["ville"];
+    _surface = j["surface"];
+    _machines = j["machines"];
   }
 };
 
